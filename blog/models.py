82ce77813b93005d41d73a27b304522ee10b43a3
TimothyJNeale <tim.neale@zenbyte.ai>
2023-01-28 12:17:02 +0000

    Added Blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField(default=date.today)
+    entry = models.TextField()
+ 
